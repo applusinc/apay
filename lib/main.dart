e1e149e2f823289bb4c9773f369bdf3ed75e0bec
Ali Abdullah Diri <aliapodiri@gmail.com>
2023-12-15 19:43:30 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
-        body: Center(child: Text("Apay")),
+        body: Center(child: Text("Apo")),
       ),
     );
   }
